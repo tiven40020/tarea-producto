9a3e797384b61f97e6285eab1eae79cc8bbe66d4
steven123 <steven40020@gmail.com>
2025-10-15 13:34:59 -0500

    Parte del frontend terminado

--- product-frontend/src/app/app.ts
+++ product-frontend/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { ProductoListComponent } from './components/producto-list/producto-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ProductoListComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('product-frontend');
+export class AppComponent {
+  title = 'product-frontend';
 }
